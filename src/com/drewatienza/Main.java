87e3f5e4912bf01c6e208732aac6f91ca1bdf4c5
drewatienza <atienza.drew@gmail.com>
2018-05-14 14:02:15 -0700

    created the outline for student class

---
+++ src/com/drewatienza/Main.java
@@ -0,0 +1,10 @@
+package com.drewatienza;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Ask how many users you want to add
+
+        // Create n number of students
+    }
+}
---
+++ src/com/drewatienza/Student.java
@@ -0,0 +1,24 @@
+package com.drewatienza;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentId;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
+
+    // Constructor: prompt user to enter student's name and year
+
+    // Generate ID
+
+    // Enroll in courses
+
+    // View balance
+
+    // Pay tuition
+
+    // Show status
+
+}
